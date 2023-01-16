835cc8597d766d9842b989fb18ba26ff2980ef28
futuresea-dev <futuresea.dev@gmail.com>
2023-01-16 10:28:20 -0800

    update the code with google drive sdk

--- nasa_articles.rb
+++ nasa_articles.rb
@@ -9,21 +9,22 @@ require 'json'
 # Functioin: Scraping article data from www.nasa.gov 
 
 def getNasaArticle(src_url)
-    content_html = URI.open(src_url)
-    parsed_data = Nokogiri::HTML.parse(content_html)
-    article_id = parsed_data.to_s.scan(/\/ubernode\/(\d+)/i)[0][0]
-    article_url = "https://www.nasa.gov/api/2/ubernode/#{article_id}"
-    article_html = URI.open(article_url)
-    json_data = Nokogiri::HTML.parse(article_html)
-    obj = JSON.parse(json_data)
+  content_html = URI.open(src_url)
+  parsed_data = Nokogiri::HTML.parse(content_html)
+  article_id = parsed_data.to_s.scan(/\/ubernode\/(\d+)/i)[0][0]
+  article_url = "https://www.nasa.gov/api/2/ubernode/#{article_id}"
+  article_html = URI.open(article_url)
+  json_data = Nokogiri::HTML.parse(article_html)
+  obj = JSON.parse(json_data)
 
-    result =  { 
-        :title => obj["_source"]["title"],
-        :date => obj["_source"]["promo-date-time"],
-        :release_no => obj["_source"]["release-id"],
-        :article => obj["_source"]["body"]
-    }
+  result =  { 
+    :title => obj["_source"]["title"],
+    :date => obj["_source"]["promo-date-time"],
+    :release_no => obj["_source"]["release-id"],
+    :article => obj["_source"]["body"]
+  }
 end
+
 puts getNasaArticle('https://www.nasa.gov/press-release/nasa-industry-to-collaborate-on-space-communications-by-2025')
 
 
--- pdf_parser.rb
+++ pdf_parser.rb
@@ -1,121 +1,121 @@
+# Ruby test #2
+
 require 'pdf-reader'
+require "google_drive"
 
 def get_info(pdf_file_path)
-    content = ""
-    result = {}
-    reader = PDF::Reader.new(pdf_file_path)
-    reader.pages.each do |page|      
-      content = content + page.text
-      break
-    end
 
-    # File.write("content.txt", content)
-    
-    
-    result[:Petitioner] = get_petitioner(content, "Petitioner") 
+  content = ""
+  result = {}
+  reader = PDF::Reader.new(pdf_file_path)
 
-    appellant = get_petitioner(content, "Appellant")
-    result[:Appellant] = appellant unless appellant.empty?
+  reader.pages.each do |page|
+    content = content + page.text
+    break
+  end
 
-    result[:Respondent] = get_repondent(content, "Respondent") 
-    
-    appellee = get_petitioner(content, "Appellee")
-    result[:Appellee] = appellee unless appellee.empty?
+  result[:Petitioner] = get_petitioner(content, "Petitioner")
+  appellant = get_petitioner(content, "Appellant")
+  result[:Appellant] = appellant unless appellant.empty?
 
-    amount = content.scan(/(\$[\d|,|\.]+)/i)[0]
-    result[:amoount] = amount[0].gsub(/,/, "").strip unless amount.nil? or amount.empty?
-    date = content.scan(/Date of Notice:(.*)/i)[0]
-    date = content.scan(/Date of Order:(.*)/i)[0] if date.nil? or date.empty?
-    date = content.scan(/Date of Judgment:(.*)/i)[0] if date.nil? or date.empty?
+  result[:Respondent] = get_repondent(content, "Respondent")
 
-    result[:date] = date[0].strip unless date.nil? or date.empty?
-    
-    result
+  appellee = get_petitioner(content, "Appellee")
+  result[:Appellee] = appellee unless appellee.empty?
+
+  amount = content.scan(/(\$[\d|,|\.]+)/i)[0]
+  result[:amoount] = amount[0].gsub(/,/, "").strip unless amount.nil? or amount.empty?
+  date = content.scan(/Date of Notice:(.*)/i)[0]
+  date = content.scan(/Date of Order:(.*)/i)[0] if date.nil? or date.empty?
+  date = content.scan(/Date of Judgment:(.*)/i)[0] if date.nil? or date.empty?
+
+  result[:date] = date[0].strip unless date.nil? or date.empty?
+
+  result
 end
 
 def get_petitioner(content, role)
     
-    if content.scan(/#{role}/i).empty?
-        return ""
-    end
-    lines = content.split("\n")
-    for line in 1..6 do
-        matches = lines[line].match(/(.*) \)/)
-        unless matches.nil?
-            break
-        end
-    end
-    if matches.nil?
-        for line in 1..6 do
-            matches = lines[line].match(/(.*)Supreme Court No./)
-            unless matches.nil?
-                break
-            end
-        end
-    end
-
-    unless matches.nil? or matches[1].nil? 
-        who = matches[1].strip.gsub(/,/, '')
-    end
-        
-    who
-   
-end
-def get_repondent(content, role)
-    
-    if content.scan(/#{role}/i).empty?
-        return ""
-    end
-    lines = content.split("\n")
-    line = 16
-
-    while line > 0
-        matches = lines[line].match(/#{role}/)
-        unless matches.nil?
-            break
-        end
-        line = line - 1
-    end
-
-    if line >= 1
-        line = line - 1
-        
-        while line > 1 && !(lines[line].scan(/Date of Judgment:/i).empty?)
-            line = line - 1
-        end
-
-        matches = lines[line].match(/(.*)/)
-        
-        unless matches.nil? && line > 1
-            if matches[0].scan(/Date of Judgment:/i)
-                line = line - 1
-            end
-            matches = lines[line].match(/(.*)/)
-        end
-        
-        unless matches.nil? or matches[1].nil? 
-            unless matches[1].scan(/(.*)Appellate Rule/).empty?
-                matches = matches[1].scan(/(.*)Appellate Rule/)[0]
-                who = matches[0].gsub(/,/, '').gsub(' )', '').strip            
-            else
-                who = matches[1].gsub(/,/, '').gsub(' )', '').strip            
-            end
-            
-        end
-        return who
-    end
-    
+  if content.scan(/#{role}/i).empty?
     return ""
-
-end
-def process_all
-    i = 0
-    Dir["*.pdf"].each do |file_name|
-        puts file_name
-        puts get_info(file_name)
-        i = i + 1
+  end
+  lines = content.split("\n")
+  for line in 1..6 do
+    matches = lines[line].match(/(.*) \)/)
+    break unless matches.nil?
+  end
+  if matches.nil?
+    for line in 1..6 do
+      matches = lines[line].match(/(.*)Supreme Court No./)
+      break unless matches.nil?
     end
+  end
+
+  unless matches.nil? or matches[1].nil? 
+    result = matches[1].strip.gsub(/,/, '')
+  end
+  result
+
 end
 
+def get_repondent(content, role)
+  
+  if content.scan(/#{role}/i).empty?
+    return ""
+  end
+  lines = content.split("\n")
+  line = 16
 
+  while line > 0
+    matches = lines[line].match(/#{role}/)
+    break unless matches.nil?
+    line = line - 1
+  end
+
+  if line >= 1
+    line = line - 1
+    
+    while line > 1 && !(lines[line].scan(/Date of Judgment:/i).empty?)
+      line = line - 1
+    end
+
+    matches = lines[line].match(/(.*)/)
+    
+    unless matches.nil? && line > 1
+      if matches[0].scan(/Date of Judgment:/i)
+        line = line - 1
+      end
+      matches = lines[line].match(/(.*)/)
+    end
+    
+    unless matches.nil? or matches[1].nil? 
+      unless matches[1].scan(/(.*)Appellate Rule/).empty?
+        matches = matches[1].scan(/(.*)Appellate Rule/)[0]
+        who = matches[0].gsub(/,/, '').gsub(' )', '').strip            
+      else
+        who = matches[1].gsub(/,/, '').gsub(' )', '').strip            
+      end
+    end
+    return who
+  end
+  return ""
+end
+
+def process_all
+  Dir["*.pdf"].each do |file_name|
+    puts get_info(file_name)
+  end
+end
+
+def download_from_google_drive
+  puts "Downloading PDFs from google drive..."
+  session = GoogleDrive::Session.from_config("config.json")
+  session.files.each do |file|
+    File.write(file.title, file.download_to_string)
+    puts "Downloaded #{file.title}"
+    break
+  end
+end
+
+download_from_google_drive
 process_all
